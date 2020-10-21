c7f72cc51139e072bdbfc73a200b5a5e2a51de12
KirkBoyd <40477730+KirkBoyd@users.noreply.github.com>
2020-10-20 22:10:12 -0400

    sync

--- lab3.m
+++ lab3.m
@@ -20,7 +20,8 @@ tripped = false; %checks if an individual node met convergence criteria
 flag = true; %flag which goes down when convergence criteria for every node is met, allowing exit of Gauss-Seidel iteration loop
 run = true;%flag variable to keep the program running
 qPrime = 0;% storage variable for q'
-qPrimeVals = zeros(14,1); %storage matrix for qPrime
+qPrime1Vals = zeros(14,1); %storage matrix for
+qPrimeConv = 0;
 nodeReq = true;
 
 %% MATRIX SETUP %%
@@ -35,6 +36,7 @@ for i=7:length(T_old)   %add T_infinity values. Optional extra, zeros can be rep
         T_old(13,i) = 0;
         T_old(14,i) = 0;
 end
+h = zeros(14,1);
 
 %% MAIN LOOP %%
 while(run)
@@ -44,7 +46,7 @@ while(run)
     %FOR DEBUG% h_0 = 1;%to add input delete this part of the comment% input("Please input value of 'h_0' in Watts per (Kelvin * Square Meters): ");%store value of h_0 in W/(K*m^2)
     h_0 = 1;%str2num(cell2mat(inputdlg('Enter value of h-0 in W/(K*m^2)','Please Input x-Value',dialogBoxDimensions,defaultInput)));
     %FOR DEBUG% convCriteria = 0.01;%to add input delete this part of the comment%input("Please input value of convergence criteria: "); %store value of convergence criteria in units of degreesC - degreesC
-    convCriteria = 0.0001%str2num(cell2mat(inputdlg('Enter value of desired Convection Criteria','Please Input x-Value',dialogBoxDimensions,defaultInput)));
+    convCriteria = 1;%str2num(cell2mat(inputdlg('Enter value of desired Convection Criteria','Please Input x-Value',dialogBoxDimensions,defaultInput)));
     for i=1:length(T_old) %%adds guess value for nodes to be populated
         T_old(i,:) = guess; 
     end %%for loop
@@ -52,15 +54,16 @@ while(run)
     for i=1:length(T_old) %add left wall boundary values
         T_old(i,1) = 300+10*(14-i);%%add left wall values where T(0,y)=(300+10y)
     end %%for loop
-
-    T_new = T_old; %create second array to store new values
+ 
+    T_new = T_old;
 
     while(flag)
-        T_old = T_new;  %at the beginning of loop, copy new values to T_old
+         %create second array to store new values
+          %at the beginning of loop, copy new values to T_old
                         %before changing the values in T_new again
         for i=1:length(T_new) %start for loop for y indices
             for j=1:length(T_new) %start for loop for x indices
-                h = h_0*(1+(j-5)/(8));
+                h(j) = h_0*(1+((j-1)-5)/(8));
                 %X=2 to x=5
                 if 1<j && j<6 %if x is between 1 and 6...
                     if i==1 % if it is a top node
@@ -70,78 +73,92 @@ while(run)
                         T_new(i,j) = (1/2)*(0.5*T_old(i,j-1) + 0.5*T_old(i,j+1) + T_old(i-1,j)); %eq. B
                     end
                     if 1<i && i<14 %if it is a central node
-                        T_new(i,j) = (1/4)*(T_old(i,j-1) + T_old(i+1,j) + T_old(i,j-1) + T_old(i-1,j));%eq. C
+                        T_new(i,j) = (1/4)*(T_old(i,j-1) + T_old(i+1,j) + T_old(i,j+1) + T_old(i-1,j));%eq. C
                     end       
                 end
                 if j==6 %if x = 6...
                     if (1<i && i<4) || (6<i && i<9) || (11<i && i<14) %if vertical convection node
-                        T_new(i,j) = (k/(2*k+h*deltaX))*(T_old(i,j-1) + 0.5*T_old(i+1,j) + 0.5*T_old(i-1,j) + ((h*deltaX)/k)*T_infinity); %eq D
+                        T_new(i,j) = (k/(2*k+h(j)*deltaX))*(T_old(i,j-1) + 0.5*T_old(i+1,j) + 0.5*T_old(i-1,j) + ((h(j)*deltaX)/k)*T_infinity); %eq D
+                        qPrimeConv = qPrimeConv + h(i)*deltaX*(T_new(i,j)-T_infinity);
                     end
                     if i==4 || i==9 %if top left fin corner
-                        T_new(i,j) = (1/(3+((h*deltaX)/k)))*(0.5*T_old(i-1,j) + T_old(i,j-1) + T_old(i+1,j) + 0.5*T_old(i,j+1) + ((h*deltaX)/k)*T_infinity); %eq. I
+                        T_new(i,j) = (1/(3+((h(j)*deltaX)/k)))*(0.5*T_old(i-1,j) + T_old(i,j-1) + T_old(i+1,j) + 0.5*T_old(i,j+1) + ((h(j)*deltaX)/k)*T_infinity); %eq. I
+                        qPrimeConv = qPrimeConv + h(i)*deltaX*(T_new(i,j)-T_infinity);
                     end
                     if i==5 || i==10 %if fin base internal node
-                        T_new(i,j) = (1/4)*(T_old(i,j-1) + T_old(i+1,j) + T_old(i,j-1) + T_old(i-1,j));%eq. C
+                        T_new(i,j) = (1/4)*(T_old(i,j-1) + T_old(i+1,j) + T_old(i,j+1) + T_old(i-1,j));%eq. C
                     end
                     if i==6 || i==11 %if bottom left fin corner
-                        T_new(i,j) = (1/(3+((h*deltaX)/k)))*(T_old(i-1,j) + T_old(i,j-1) + 0.5*T_old(i+1,j) + 0.5*T_old(i,j+1) + ((h*deltaX)/k)*T_infinity);%eq. J
+                        T_new(i,j) = (1/(3+((h(j)*deltaX)/k)))*(T_old(i-1,j) + T_old(i,j-1) + 0.5*T_old(i+1,j) + 0.5*T_old(i,j+1) + ((h(j)*deltaX)/k)*T_infinity);%eq. J
+                        qPrimeConv = qPrimeConv + h(i)*deltaX*(T_new(i,j)-T_infinity);
                     end
                     if i==1 %if top corner node (y=1)
-                        T_new(i,j) = (1/(2*k+h*deltaX))*(k*T_old(i,j-1) + k*T_old(i+1,j) + h*deltaX*T_infinity);%eq. G
+                        T_new(i,j) = (1/(2*k+h(j)*deltaX))*(k*T_old(i,j-1) + k*T_old(i+1,j) + h(j)*deltaX*T_infinity);%eq. G
+                        qPrimeConv = qPrimeConv + h(i)*deltaX*(0.5)*(T_new(i,j)-T_infinity);
                     end
                     if i==14 %if bottom corner node (y=14)
-                        T_new(i,j) = (1/(2*k+h*deltaX))*(k*T_old(i,j-1) + k*T_old(i-1,j) + h*deltaX*T_infinity);%eq. H
+                        T_new(i,j) = (1/(2*k+h(j)*deltaX))*(k*T_old(i,j-1) + k*T_old(i-1,j) + h(j)*deltaX*T_infinity);%eq. H
+                        qPrimeConv = qPrimeConv + h(i)*deltaX*(0.5)*(T_new(i,j)-T_infinity);
                     end
                 end
                 if 6<j && j<14 %if x is between 6 and 14
                     if i==4 || i==9 %if top fin surface node
-                        T_new(i,j) = (1/(2*k+h*deltaX))*((k/2)*T_old(i,j-1) + k*T_old(i+1,j) + (k/2)*T_old(i,j+1) + h*deltaX*T_infinity);%eq. E
+                        T_new(i,j) = (1/(2*k+h(j)*deltaX))*((k/2)*T_old(i,j-1) + k*T_old(i+1,j) + (k/2)*T_old(i,j+1) + h(j)*deltaX*T_infinity);%eq. E
+                        qPrimeConv = qPrimeConv + h(i)*deltaX*(T_new(i,j)-T_infinity);
                     end %if i==4...
                     if i==5 || i==10 %if fin internal node
-                        T_new(i,j) = (1/4)*(T_old(i,j-1) + T_old(i+1,j) + T_old(i,j-1) + T_old(i-1,j));%eq. C
+                        T_new(i,j) = (1/4)*(T_old(i,j-1) + T_old(i+1,j) + T_old(i,j+1) + T_old(i-1,j));%eq. C
                     end %if i==5
                     if i==6 || i==11 %if bottom fin surface node
-                        T_new(i,j) = (1/(2*k+h*deltaX))*((k/2)*T_old(i,j-1) + k*T_old(i-1,j) + (k/2)*T_old(i,j+1) + h*deltaX*T_infinity); %eq. F
+                        T_new(i,j) = (1/(2*k+h(j)*deltaX))*((k/2)*T_old(i,j-1) + k*T_old(i-1,j) + (k/2)*T_old(i,j+1) + h(j)*deltaX*T_infinity); %eq. F
+                        qPrimeConv = qPrimeConv + h(i)*deltaX*(T_new(i,j)-T_infinity);
                     end %if i==6
                 end %if 6<j...
                 if j==14 %if it is a far right node
                     if i==4 || i==9 %if fin top right corner
-                        T_new(i,j) = (1/(1+((h*deltaX)/k)))*(0.5*T_old(i,j-1) + 0.5*T_old(i+1,j) + ((h*deltaX)/k)*T_infinity); %eq. K
+                        T_new(i,j) = (1/(1+((h(j)*deltaX)/k)))*(0.5*T_old(i,j-1) + 0.5*T_old(i+1,j) + ((h(j)*deltaX)/k)*T_infinity); %eq. K
+                        qPrimeConv = qPrimeConv + h(i)*deltaX*(T_new(i,j)-T_infinity);
                     end %if i==4...
                     if i==5 || i==10 %if fin right vertical
-                        T_new(i,j) = (k/(2*k+h*deltaX))*(T_old(i,j-1) + 0.5*T_old(i+1,j) + 0.5*T_old(i-1,j) + ((h*deltaX)/k)*T_infinity); %eq D
+                        T_new(i,j) = (k/(2*k+h(j)*deltaX))*(T_old(i,j-1) + 0.5*T_old(i+1,j) + 0.5*T_old(i-1,j) + ((h(j)*deltaX)/k)*T_infinity); %eq D
+                        qPrimeConv = qPrimeConv + h(i)*deltaX*(T_new(i,j)-T_infinity);
                     end %if i==5
                     if i==6 || i==11 %if fin bottom right corner
-                        T_new(i,j) = (1/(1+((h*deltaX)/k)))*(0.5*T_old(i,j-1) + 0.5*T_old(i-1,j) + ((h*deltaX)/k)*T_infinity); %eq. L
+                        T_new(i,j) = (1/(1+((h(j)*deltaX)/k)))*(0.5*T_old(i,j-1) + 0.5*T_old(i-1,j) + ((h(j)*deltaX)/k)*T_infinity); %eq. L
+                        qPrimeConv = qPrimeConv + h(i)*deltaX*(T_new(i,j)-T_infinity);
                     end %if i==6
                 end %if j==14
                 if j==1 %calculate q' using left wall
                     if i==1 || i==14
-                        qPrimeVals(i) = 0.5*k*(T_new(i,j+1)-T_new(i,j));
+                        qPrime1Vals(i) = 0.5*k*(-T_new(i,j+1)+T_new(i,j));
                     else
-                        qPrimeVals(i) = k*(T_new(i,j+1)-T_new(i,j));
+                        qPrime1Vals(i) = k*(-T_new(i,j+1)+T_new(i,j));
                     end
                 end
-                
-                %Check convergence criteria
+                                        
+                %Check convergence criteria:
                 if abs(T_new(i,j)-T_old(i,j))>convCriteria
                     tripped = true; %if any value is above conv criteria, this 
                                     %becomes true and flag will stay up
+                    qPrimeConv = 0;
                 end %if abs(...
             end %for j
         end %for i
-
+       
        count = count +1;
        if tripped
            flag = true;
+           T_old = T_new;
        else
            flag = false;
-           for i=1:length(qPrimeVals)
-               qPrime = qPrime+qPrimeVals(i);
+           for i=1:length(qPrime1Vals)
+               qPrime = qPrime+qPrime1Vals(i);
            end
        end %if/else
        tripped = false; %reset tripped for next iteration
     end %while flag
+    
+    
     
     %% PRINT FINAL VALUES %%
     fprintf('\t\tBase Temperature: \tTip Temperature:\n');
@@ -174,10 +191,15 @@ while(run)
     fprintf('\n');
     
     %PRINT q'%
+    fprintf('\nq''through left wall: ');
+    fprintf('%4.4f',qPrime);
+    fprintf('\nq''through convecting surface: ');
+    fprintf('%4.4f',qPrimeConv);
     
     %PRINT NUMBER OF ITERATIONS%
-    fprintf('Number of Iterations: ');
+    fprintf('\nNumber of Iterations: ');
     fprintf('%5d', count);
+    fprintf('\n');
     %% POST PROCESS USER INTERFACE %%
     while nodeReq
         answer1 = questdlg('Would you like to see another node value?', 'Want More Info?', 'Yes', 'No', 'No');
